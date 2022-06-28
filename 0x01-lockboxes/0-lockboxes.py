8f2d026d30b92fac82810530ae1303364455e05a
mitakeey <mitakeeykiage@gmail.com>
2022-06-28 20:46:36 +0300

    :alembic: experiments

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
-'''
-Returns a list representing the Pascal's triangle of n
-'''
 
+def canUnlockAll(boxes):
+    '''
+    determines if all the boxes can be opened 
+    '''
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
 
-def pascal_triangle(n):
-    '''returns empty list if n <= 0'''
-    if n <= 0:
-        return []
+    while i < length:
+        box = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if box != i:
+            continue
+        else:
+            break
 
-    triangle = []
-    row = []
-    prev_row = []
-    for i in range(0, n + 1):
-        row = [j > 0 and j < i - 1 and i > 2 and prev_row[j-1] +
-               prev_row[j] or 1 for j in range(0, i)]
-        prev_row = row
-        triangle += [row]
-    return triangle[1:]
-    
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
